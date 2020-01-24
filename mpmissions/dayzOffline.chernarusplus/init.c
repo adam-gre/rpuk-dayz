7ad368550c2b7c63b0ea4c8539760300346285ff
jamm1612 <jammroberts1997@gmail.com>
2020-01-24 16:16:25 +0000

    Disabled admin panel
    Bug with admin panel, needs sorting

--- mpmissions/dayzOffline.chernarusplus/init.c
+++ mpmissions/dayzOffline.chernarusplus/init.c
@@ -105,7 +105,7 @@ Mission CreateCustomMission(string path)
 
 ///////////// WORK IN PROGRESS ADMIN PANEL ///////////////
 
-	{     bool freecam_active = false;
+ /*	{     bool freecam_active = false;
 	bool verify_admins = false; // true=verify presence of BI UID in admin list
 	string cmd_prefix = "/"; // Must be special character
 	ref TStringArray admins = {76561198183828141}; // Add your BI UID or SteamID
@@ -456,7 +456,7 @@ Mission CreateCustomMission(string path)
 			}
 			///////////////////// */
 
-			case "kill": {
+		/*	case "kill": {
 				if(count == 2) {
 					temp_player = GetPlayer(tokens[1]);
 					if(temp_player == NULL) {
