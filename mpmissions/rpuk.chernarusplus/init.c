c203de627a57aeaaceb8612e8cea4767ecfa46df
jamm1612 <jammroberts1997@gmail.com>
2020-03-03 16:45:47 +0000

    Added another trader

--- mpmissions/rpuk.chernarusplus/init.c
+++ mpmissions/rpuk.chernarusplus/init.c
@@ -1,3 +1,27 @@
+#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\Buildings.c"
+
+
+void SpawnObject(string objectName, vector position, vector orientation)
+{
+    Object obj;
+    obj = Object.Cast(GetGame().CreateObject(objectName, "0 0 0"));
+    obj.SetPosition(position);
+    obj.SetOrientation(orientation);
+ 
+    // Force update collisions
+    if (obj.CanAffectPathgraph())
+    {
+        obj.SetAffectPathgraph(true, false);
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(GetGame().UpdatePathgraphRegionByObject, 100, false, obj);
+    }
+}
+
+<!------------------------------------------------------------------------------------------!>
+
+	//INIT BUILDINGS
+	AddBuildings();	
+		//GetCEApi().ExportProxyData("7500 0 7500", 10000);  //Centre of map, radius of how far to go out and find buildings.
+
 void main()
 {
 	//INIT WEATHER BEFORE ECONOMY INIT------------------------
